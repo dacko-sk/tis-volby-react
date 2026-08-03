245d3bcda9a0ad0ff7aaf4bd0bdf444ecc40407d
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2026-08-04 00:54:13 +0200

    fix combined news for missing wp tag

--- src/pages/samosprava2026/party/PartyNews.jsx
+++ src/pages/samosprava2026/party/PartyNews.jsx
@@ -3,7 +3,10 @@ import { useLocation, useOutletContext } from 'react-router';
 import { setTitle } from '../../../helpers/browser';
 import { labels, t } from '../../../helpers/dictionary';
 import { wpCat } from '../../../helpers/wp';
-import { findSubjectByPathname, cmsSubsitesMap } from '../../../hooks/CmsQueries';
+import {
+    findSubjectByPathname,
+    cmsSubsitesMap,
+} from '../../../hooks/CmsQueries';
 
 import AlertWithIcon from '../../../components/general/AlertWithIcon';
 import CombinedNews from '../../../components/news/CombinedNews';
@@ -14,18 +17,19 @@ function PartyNews() {
 
     const cmsSubject = findSubjectByPathname(cmsData, pathname);
 
-    const content = cmsSubject?.primaryParty?.wpTag ? (
-        <CombinedNews
-            election={cmsSubsitesMap.samosprava2026}
-            categories={[wpCat.news]}
-            tags={[cmsSubject?.primaryParty?.wpTag]}
-            party={cmsSubject?.primaryParty?.uid}
-        />
-    ) : (
-        <AlertWithIcon className="my-4" variant="danger">
-            {t(labels.news.noData)}
-        </AlertWithIcon>
-    );
+    const content =
+        cmsSubject?.primaryParty?.wpTag || cmsSubject?.primaryParty?.uid ? (
+            <CombinedNews
+                election={cmsSubsitesMap.samosprava2026}
+                categories={[wpCat.news]}
+                tags={[cmsSubject?.primaryParty?.wpTag ?? 'dummy-tag']}
+                party={cmsSubject?.primaryParty?.uid}
+            />
+        ) : (
+            <AlertWithIcon className="my-4" variant="danger">
+                {t(labels.news.noData)}
+            </AlertWithIcon>
+        );
 
     setTitle(`${cmsSubject?.name} : ${t(labels.news.pageTitle)}`);
 
